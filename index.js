0eab06e02b447a18d45ed47e835f31d5c99693ec
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-03-30 22:22:50 +0800

    Added handler for message

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ const bot = new Discord.Client();
 bot.on('ready', () => {
   console.log('Connected');
 });
+bot.on('message', msg => {
+  if(msg.member.id == '693108033601011762') {
+    return;
+  }
+});
 bot.login(token);
