a7f7b482b3de9ec74bd6b60685ae9048bcf7e7c7
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-04-02 21:14:49 +0800

    Added redis implementation

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const Discord = require('discord.js');
-const token = process.env.DISCORD_BOT_RIPOSTE_TOKEN;
+const redis = require('redis').createClient();
+
+const { promisify } = require('util');
+const getAsync = promisify(redis.get).bind(redis);
+
 const spiels = require('./spiels.json').spiels;
+const token = process.env.DISCORD_BOT_RIPOSTE_TOKEN;
 
 const bot = new Discord.Client();
-bot.on('ready', () => {
-  console.log('Connected');
-});
+
+bot.on('ready', () => { console.log('Connected'); });
 bot.on('message', msg => {
-  if(msg.member.id == '693108033601011762') {
+  if(msg.member.id == bot.user.id) {
     return;
   }
+
+  const guildId = msg.member.guild.id;
+  redis.set(guildId, 'henlo');
+  redis.get(guildId, value => {
+    console.log('value');
+    console.log(value);
+  });
+
+  console.log(msg.member.guild.id);
   const spiel = spiels.find(o => o.key.toLowerCase() === msg.content.toLowerCase());
   if(spiel) {
-    console.log(msg);
     console.log(`${msg.createdAt} [${msg.member.displayName}]:  ${msg.content}`);
     msg.reply(spiel.value);
   }
