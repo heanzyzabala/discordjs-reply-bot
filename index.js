6ab78032a9479e701345840ebec0ce0895f786e9
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-04-02 23:19:02 +0800

    Replaced redis with mongodb

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const Discord = require('discord.js');
-const redis = require('redis').createClient();
-
-const { promisify } = require('util');
-const getAsync = promisify(redis.get).bind(redis);
+const MongoClient = require('mongodb').MongoClient;
 
 const spiels = require('./spiels.json').spiels;
 const token = process.env.DISCORD_BOT_RIPOSTE_TOKEN;
+const mongodbUrl = 'mongodb://localhost:27017/';
 
 const bot = new Discord.Client();
 
-bot.on('ready', () => { console.log('Connected'); });
+bot.on('ready', () => { 
+  console.log('Connected to discord');
+});
+
 bot.on('message', msg => {
   if(msg.member.id == bot.user.id) {
     return;
   }
 
   const guildId = msg.member.guild.id;
-  redis.set(guildId, 'henlo');
-  redis.get(guildId, value => {
-    console.log('value');
-    console.log(value);
+  MongoClient.connect(mongodbUrl, { useUnifiedTopology: true }, (err, client) => {
+    if(err) throw err;
+    const db = client.db('riposte');
+    db.createCollection('spiels', (err, res) => {
+      if(err) throw err;
+      console.log('Created collection');
+    });
+    db.collection('spiels').find();
+    // console.log(spiels);
+    // client.close();
   });
 
-  console.log(msg.member.guild.id);
-  const spiel = spiels.find(o => o.key.toLowerCase() === msg.content.toLowerCase());
-  if(spiel) {
-    console.log(`${msg.createdAt} [${msg.member.displayName}]:  ${msg.content}`);
-    msg.reply(spiel.value);
-  }
+  // const spiel = spiels.find(o => o.key.toLowerCase() === msg.content.toLowerCase());
+  // if(spiel) {
+  //   console.log(`${msg.createdAt} [${msg.member.displayName}]:  ${msg.content}`);
+  //   msg.reply(spiel.value);
+  // }
 });
 bot.login(token);
