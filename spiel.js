5991d18e83aec8523a77df06a5d8e2ae1abd89be
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-04-03 22:08:29 +0800

    Added save spiel function

--- spiel.js
+++ spiel.js
@@ -6,10 +6,20 @@ const Spiel = function() {
     MongoClient.connect(mongodbUrl, { useUnifiedTopology: true }, (err, client) => {
       if(err) throw err;
       const db = client.db('riposte');
-      const spiels = db.collection('spiels').find({ guild_id: { $eq: guildId } });
+      const spiels = db.collection('spiels').find({ guild_id: guildId });
       client.close();
       cb(spiels);
     });
-  }  
+  }
+
+  this.save = function(guildId, spiel, cb) {
+    MongoClient.connect(mongodbUrl, { useUnifiedTopology: true }, (err, client) => {
+      if(err) throw err;
+      const db = client.db('riposte');
+      db.collection('spiels').findAndModify({ guild_id: guildId, });
+      client.close();
+      cb(spiels);
+    });
+  }
 }
 module.exports = Spiel;
