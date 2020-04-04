22aec5e113b15c8bb151a898be38295fbb0a393f
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-04-04 17:06:45 +0800

    Added dotenv; Added functional save

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const Spiels = require('./spiels');
 
@@ -7,17 +8,31 @@ const bot = new Discord.Client();
 bot.on('ready', () => { console.log('Connected to discord'); });
 
 bot.on('message', msg => {
-  if(msg.member.id == bot.user.id) {
+  if (msg.member.id == bot.user.id) {
     return;
   }
   const guildId = msg.member.guild.id;
   const message = msg.content;
-  console.log(`guildId: ${guildId}`);
-  Spiels.find(guildId, message, function(reply) {
-    if(reply) {
-      console.log(`${msg.createdAt} [${msg.member.displayName}]:  ${msg.content}`);
-      msg.reply(reply);
+  const splt_message = message.split(' ');
+  if (splt_message[0] === '!!save') {
+    if(splt_message.length != 3) {
+      msg.reply('Invalid arguments, should be: ~save <key> <value>');
+    } else {
+      Spiels.save(guildId, {
+        key: splt_message[1],
+        value: splt_message[2]
+      }, function (res) {
+        console.log(res);
+      });
     }
-  });
+  } else {
+    console.log(`guildId: ${guildId}`);
+    Spiels.find(guildId, message, function (reply) {
+      if (reply) {
+        console.log(`${msg.createdAt} [${msg.member.displayName}]:  ${msg.content}`);
+        msg.reply(reply);
+      }
+    });
+  }
 });
 bot.login(token);
--- spiels.js
+++ spiels.js
@@ -1,3 +1,4 @@
+// TOOD: Optimize mongo queries
 const Spiels = function() {
   const MongoClient = require('mongodb').MongoClient;
   const mongodbUrl = 'mongodb://localhost:27017/';
@@ -10,22 +11,31 @@ const Spiels = function() {
       if(spiel) {
         const mappings = spiel.mappings;
         const reply = mappings.find(m => { return m.key === message; });
-        cb(reply.value);
+        if(reply) {
+          cb(reply.value);
+        } e
       }
       client.close();
     });
   }
 
   this.save = function(guildId, mapping, cb) {
-    MongoClient.connect(mongodbUrl, { useUnifiedTopology: true }, (err, client) => {
+    MongoClient.connect(mongodbUrl, { useUnifiedTopology: true }, async (err, client) => {
       if(err) throw err;
       const db = client.db('riposte');
       const spiel = await db.collection('spiels').findOne({ guild_id: guildId });
       if(spiel) {
-        const mappings = spiel.mappings:
-        const index = mappings.findIndex(m => m.key === )
+        const mappings = spiel.mappings;
+        console.log(mappings);
+        const index = mappings.findIndex(m => m.key === mapping.key);
+        if(index === -1) {
+          mappings.push(mapping);
+        } else {
+          mappings[index] = mapping;
+        }
+        cb(await db.collection('spiels').update({ guild_id: guildId, mappings: [ mappings] }));
       } else {
-        await db.collection.insert({ guild_id: guildId, mappings: [ mapping ] });
+        cb(await db.collection('spiels').insertOne({ guild_id: guildId, mappings: [ mapping ] }));
       }
       client.close();
     });
