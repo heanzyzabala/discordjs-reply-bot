a4d02668090f739c5b0a3ac9acf1b47823d0805b
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-05-02 17:11:05 +0800

    Added more tests

--- test/commands/remove.test.js
+++ test/commands/remove.test.js
@@ -1,13 +1,24 @@
+/* eslint-disable no-loop-func */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 const chai = require('chai');
 const sinon = require('sinon');
 
 const { expect } = chai;
 
-const Add = require('../../src/commands/remove');
+const Remove = require('../../src/commands/remove');
 const Views = require('../../src/views');
 
 describe('remove', () => {
   it('should return name', () => {
-    expect(Add.name).to.equal('remove');
+    expect(Remove.name).to.equal('remove');
+  });
+  it('should return aliases', () => {
+    expect(Remove.aliases).to.deep.equal(['r']);
+  });
+  it('should return usage', () => {
+    expect(Remove.usage).to.equal('"<key>"|<index>');
   });
 });
