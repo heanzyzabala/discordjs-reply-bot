a2e9c13fbf67c5671de8aa4fee533f999d981cf4
Heanzy Zabala <heanzy.zabala@siteminder.com>
2020-12-08 13:24:56 +0800

    Added ormconfig.json

--- src/db.ts
+++ src/db.ts
@@ -1,18 +1,7 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import { Reply } from './entities/reply';
 
 const connect = async () => {
-  await createConnection({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'thepasswordofdoom',
-    database: 'discord-reply-bot-db',
-    entities: [Reply],
-    synchronize: true,
-    logging: false,
-  });
+  await createConnection();
 };
 export default connect();
