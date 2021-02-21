339fc232933d512dc5f8a152cc0fd3827183a49e
Heanzy Zabala <heanzy.zabala@siteminder.com>
2021-02-21 21:51:22 +0800

    Added common

--- src/common.ts
+++ src/common.ts
@@ -1,25 +1,24 @@
 import * as Bunyan from 'bunyan';
 
-const rootLogger = (): Bunyan => {
-	const logger = Bunyan.createLogger({
-		name: 'discord-reply-bot',
+export const createLogger = (): Bunyan => {
+	const rootLogger = Bunyan.createLogger({
+		name: 'discordjs-reply-bot',
+		src: true,
 		streams: [
 			{
 				level: 'info',
-				path: '/var/log/discordjs-reply-bot/info.log',
+				path: process.env.LOG_ROOT_DIR + '/discordjs-reply-bot/info.log',
 			},
 			{
 				level: 'error',
-				path: '/var/log/discordjs-reply-bot/error.log',
+				path: process.env.LOG_ROOT_DIR + '/discordjs-reply-bot/error.log',
 			},
-		],
-	});
-
+		]
+	})
 	if (process.env.NODE_ENV === 'dev') {
-		logger.addStream({ level: 'info', stream: process.stdout });
-		logger.addStream({ level: 'error', stream: process.stderr });
-		logger.info('Adding dev log stream');
+		rootLogger.addStream({ level: 'info', stream: process.stdout });
+		rootLogger.addStream({ level: 'error', stream: process.stderr });
+		rootLogger.debug({}, 'Adding local log stream')
 	}
-	return logger;
-};
-export { rootLogger };
+	return rootLogger;
+}
